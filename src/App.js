e6a3e2ca0a5e3df8a8fba72138e8775b0ed39d1e
elayeboussama <61306430+elayeboussama@users.noreply.github.com>
2021-08-16 13:24:59 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import React from 'react';
+// import React, { useState } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Redirect,
+//   Switch
+// } from 'react-router-dom';
+
+import Home from './Components/home/Home.js';
+
+
+
+const App = () => {
+
+  return (
+    <div className="App" >
+      <Home />
+    </div>
+
+  );
+}
+
+export default App;
